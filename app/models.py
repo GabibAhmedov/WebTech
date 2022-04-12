d73b35f2afa462f3fe5d42a8c001043a537e681c
Gabib <gabib2002@mail.ru>
2022-04-12 22:28:24 +0300

    model question

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class LikeSection(models.Model):
+    likes = models.
+    dislikes =
+
+class Question(models.Model):
+    title = models.CharField(max_length=60)
+    like = models.ForeignKey(LikeSection, models.PROTECT)
+
+    # Create your models here.
