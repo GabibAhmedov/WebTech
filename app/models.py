529b59f6c4a6bfd97aef0988a90dc7418554d4cb
Gabib <gabib2002@mail.ru>
2022-04-12 22:32:31 +0300

    Revert "model question"
    This reverts commit d73b35f2afa462f3fe5d42a8c001043a537e681c.

--- app/models.py
+++ app/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 
-
-class LikeSection(models.Model):
-    likes = models.
-    dislikes =
-
-class Question(models.Model):
-    title = models.CharField(max_length=60)
-    like = models.ForeignKey(LikeSection, models.PROTECT)
-
-    # Create your models here.
+# Create your models here.
