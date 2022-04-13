b548e4f5282090545760bc984e16dc9febbbf69e
Gabib <gabib2002@mail.ru>
2022-04-13 03:14:55 +0300

    fixed tags

--- app/views.py
+++ app/views.py
@@ -1,10 +1,9 @@
 import string
 from random import random, randrange
+import random
 
 from django.shortcuts import render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-
-# Create your views here.
 
 QUESTIONS = [
     {
@@ -12,7 +11,6 @@ QUESTIONS = [
         "text": f"This is text for question #{i}",
         "number": i,
     } for i in range(100)
-
 ]
 
 ANSWERS = [
@@ -20,18 +18,60 @@ ANSWERS = [
         "title": f"Answer#{i}",
         "text": f"This is text for answer #{i}",
         "number": i,
-    } for i in range(10)
+    } for i in range(100)
 ]
+TWO_TAGS = [
 
+    {
+        "title": "Bee",
+        "color": "#" + ''.join([random.choice('ABCDEF0123456789') for i in range(6)])
+    },
+    {
+        "title": "Honey",
+        "color": "#" + ''.join([random.choice('ABCDEF0123456789') for i in range(6)])
+    }
+
+]
 TAGS = [
     {
-        "title": f"Answer#{i}",
-        "text": f"This is text for answer #{i}",
-        "number": i,
-    } for i in range(10)
+        "title": "Bee",
+        "color": "#" + ''.join([random.choice('ABCDEF0123456789') for i in range(6)])
+    },
+    {
+        "title": "Honey",
+        "color": "#" + ''.join([random.choice('ABCDEF0123456789') for i in range(6)])
+    },
+    {
+        "title": "Flower",
+        "color": "#" + ''.join([random.choice('ABCDEF0123456789') for i in range(6)])
+    },
+    {
+        "title": "Hive",
+        "color": "#" + ''.join([random.choice('ABCDEF0123456789') for i in range(6)])
+    },
+    {
+        "title": "Sting",
+        "color": "#" + ''.join([random.choice('ABCDEF0123456789') for i in range(6)])
+    },
+    {
+        "title": "Foam",
+        "color": "#" + ''.join([random.choice('ABCDEF0123456789') for i in range(6)])
+    },
+    {
+        "title": "Foam",
+        "color": "#" + ''.join([random.choice('ABCDEF0123456789') for i in range(6)])
+    },
+    {
+        "title": "Foam",
+        "color": "#" + ''.join([random.choice('ABCDEF0123456789') for i in range(6)])
+    },
+    {
+        "title": "Foam",
+        "color": "#" + ''.join([random.choice('ABCDEF0123456789') for i in range(6)])
+    },
 ]
 
-HOT_QUESTIONS= [
+HOT_QUESTIONS = [
     {
         "likes": randrange(24, 55),
         "dislikes": randrange(3, 17),
@@ -40,38 +80,40 @@ HOT_QUESTIONS= [
 ]
 
 
-
 def paginate(objects_list, request, per_page=10):
-    question_paginator = Paginator(objects_list, per_page)
+    paginator = Paginator(objects_list, per_page)
     page_num = request.GET.get('page')
-    page = question_paginator.get_page(page_num)
+    page = paginator.get_page(page_num)
     return page
 
 
 def index(request):
-    page = paginate(QUESTIONS,request)
-    return render(request, "index.html", {"questions": QUESTIONS, "page": page})
+    page = paginate(QUESTIONS, request)
+    return render(request, "index.html", {"questions": QUESTIONS, "page": page, "tags": TAGS,"two_tags": TWO_TAGS})
 
 
 def tag(request, s: string):
-    return render(request, "tag.html", {"questions": QUESTIONS, "tag": s})
+    return render(request, "tag.html", {"questions": QUESTIONS, "tag": s, "tags": TAGS})
 
 
 def ask(request):
-    return render(request, "ask.html")
+    return render(request, "ask.html", {"tags": TAGS})
 
 
 def signup(request):
-    return render(request, "signup.html")
+    return render(request, "signup.html", {"tags": TAGS})
 
 
 def login(request):
-    return render(request, "login.html")
+    return render(request, "login.html", {"tags": TAGS})
 
 
 def question(request, i: int):
-    return render(request, "question.html", {"question": QUESTIONS[i], "answers": ANSWERS})
+    page = paginate(ANSWERS, request)
+    return render(request, "question.html",
+                  {"question": QUESTIONS[i], "answers": ANSWERS, "tags": TAGS, "page": page, "index": i,
+                   "two_tags": TWO_TAGS})
 
 
 def hot(request):
-    return render(request, "index.html", {"questions": QUESTIONS, "hot_questions": HOT_QUESTIONS})
+    return render(request, "index.html", {"questions": QUESTIONS, "hot_questions": HOT_QUESTIONS, "tags": TAGS})
